95fe81e4c08e931684cb2dc3398dc2a315414fd2
Tamani Wolf <VinylWolf.mail@gmail.com>
2021-10-30 21:42:22 +0200

    Update twitchlink.js

--- ClanCore/Modules/twitch/twitchlink.js
+++ ClanCore/Modules/twitch/twitchlink.js
@@ -3,29 +3,8 @@
 module.exports = (client, twitchClient, args, Discord, message) => {
     require('dotenv').config();
     const configmain = require('../../../ClanSys/config/config.json');
-    // const tmi = require('tmi.js');
-    // const twitchClient = new tmi.Client({
-    //     options: { debug: true, messagesLogLevel: "info" },
-    //     connection: {
-    //         reconnect: true,
-    //         secure: true
-    //     },
-
-    //     identity: {
-    //         username: `${process.env.TWITCH_USERNAME}`,
-    //         password: `oauth:${process.env.TWITCH_OAUTH}`
-    //     },
-
-    //     channels: [`${process.env.TWITCH_CHANNEL}`]
-    // });
-
-    // twitchClient.connect().catch(console.error);
-    // console.log(message)
     twitchClient.on('message', (channel, userstate, message2, self) => {
         if(self) return;
-        client.channels.cache.get('846716632155816016').send(`\`${userstate['display-name']}:\` ${message2}`)
-        // console.log(message);
-        // if(message.author.id === configmain.botid) return;
-        // twitchClient.say(message);
+        client.channels.cache.get(configmain.discord_channel).send(`\`${userstate['display-name']}:\` ${message2}`)
     })
 };
